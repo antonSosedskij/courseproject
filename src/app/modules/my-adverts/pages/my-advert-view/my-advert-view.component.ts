f2257a30d8323c6918230ee4d35e22cf27decd00
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-04-07 01:04:25 +0300

    create my-ad view component

---
+++ src/app/modules/my-adverts/pages/my-advert-view/my-advert-view.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-advert-view',
+  templateUrl: './my-advert-view.component.html',
+  styleUrls: ['./my-advert-view.component.scss']
+})
+export class MyAdvertViewComponent {
+
+}
