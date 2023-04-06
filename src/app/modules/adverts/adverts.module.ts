dd231acac03685116fbc51f022dc004ec61dc7b2
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-04-07 01:10:44 +0300

    add ads-recomendations module

--- src/app/modules/adverts/adverts.module.ts
+++ src/app/modules/adverts/adverts.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AdvertsRecComponent } from './pages/adverts-rec/adverts-rec.component';
+import { AdvertRecViewComponent } from './pages/advert-rec-view/advert-rec-view.component';
+import { AdvertCardModule } from 'src/app/shared/components/advert-card/advert-card.component';
+import { AdvertsRoutingModule } from './adverts-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AdvertsRecComponent,
+    AdvertRecViewComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    AdvertCardModule,
+    AdvertsRoutingModule
   ]
 })
 export class AdvertsModule { }
