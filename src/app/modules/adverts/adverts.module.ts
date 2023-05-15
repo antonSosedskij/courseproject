40f99485378c2e737f4352f411dbe084e24e85f1
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-05-15 13:47:48 +0300

    add imports

--- src/app/modules/adverts/adverts.module.ts
+++ src/app/modules/adverts/adverts.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgIf } from '@angular/common';
 import { AdvertsRecComponent } from './pages/adverts-rec/adverts-rec.component';
 import { AdvertRecViewComponent } from './pages/advert-rec-view/advert-rec-view.component';
 import { AdvertCardModule } from 'src/app/shared/components/advert-card/advert-card.component';
 import { AdvertsRoutingModule } from './adverts-routing.module';
 import {ViewAdvertModule } from 'src/app/shared/components/view-advert/view-advert.component';
+import { SkeletonModule } from 'primeng/skeleton';
 
 
 
@@ -17,7 +18,9 @@ import {ViewAdvertModule } from 'src/app/shared/components/view-advert/view-adve
     CommonModule,
     AdvertCardModule,
     AdvertsRoutingModule,
-    ViewAdvertModule
+    ViewAdvertModule,
+    SkeletonModule,
+    NgIf
   ]
 })
 export class AdvertsModule { }
