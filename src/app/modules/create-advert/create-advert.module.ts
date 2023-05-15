bbc037996ba84be6c28e8330ce2bfd29af2bfd09
dev.sosedskiy <dev.sosedskiy@gmail.com>
2023-05-15 13:47:32 +0300

    add imports

--- src/app/modules/create-advert/create-advert.module.ts
+++ src/app/modules/create-advert/create-advert.module.ts
@@ -2,16 +2,24 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CreateAdvertComponent } from './pages/create-advert/create-advert.component';
 import { CreateAdvertRoutingModule } from './create-advert-routing.module';
-
-
+import { InputTextModule } from 'primeng/inputtext';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { ButtonModule } from 'primeng/button';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FileUploadComponent } from './components/file-upload/file-upload.component';
 
 @NgModule({
   declarations: [
-    CreateAdvertComponent
+    CreateAdvertComponent,
+    FileUploadComponent
   ],
   imports: [
     CommonModule,
-    CreateAdvertRoutingModule
+    CreateAdvertRoutingModule,
+    InputTextModule,
+    InputNumberModule,
+    ButtonModule,
+    ReactiveFormsModule
   ]
 })
 export class CreateAdvertModule { }
